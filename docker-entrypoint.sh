5426c5f1838831d6b9a6211919567fa950869e79
Reuven Harrison <reuvenharrison@gmail.com>
2025-12-14 21:19:17 +0200

    Fixed Docker setup for the project

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 # Use PORT environment variable if set, otherwise default to 8080
-PORT=${PORT:-8080}
+export PORT=${PORT:-8080}
+
+echo "Starting nginx on port ${PORT}"
 
 # Update nginx config with the correct port
-sed -i "s/listen 8080;/listen ${PORT};/g" /etc/nginx/conf.d/default.conf
-sed -i "s/listen \[::\]:8080;/listen [::]:${PORT};/g" /etc/nginx/conf.d/default.conf
+sed -i "s/listen 8080/listen ${PORT}/g" /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
